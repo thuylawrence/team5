08973d171c363e8a8db157f97e7c4070ba68cd8a
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-12-05 19:29:55 -0700

    Add tags to swagger docs

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,27 @@ const router = require('express').Router();
 const users = require('./users');
 const hobbies = require('./hobbies');
 const favorites = require('./favorites');
+const skillset = require("./skillset")
 
-
-router.use('/users', users);
-router.use('/hobbies', hobbies);
-router.use('/favorites', favorites);
+router.use(
+    // #swagger.tags = ['Users']
+    '/users',
+    users
+);
+router.use(
+    // #swagger.tags = ['Hobbies']
+    '/hobbies',
+    hobbies
+);
+router.use(
+    // #swagger.tags = ['Favorites']
+    '/favorites',
+    favorites
+);
+router.use(
+    // #swagger.tags = ['Skills']
+    '/skillset',
+    skillset
+)
 
 module.exports = router;
